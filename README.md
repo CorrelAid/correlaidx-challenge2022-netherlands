624407d3257649d10c90e1e453870af31c4e0a69
David Valpey <greengargoyle@hotmail.com>
2023-01-10 18:42:32 +0100

    Add some documentation to README.md and functions

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # correlaidx-challenge2022-netherlands
+
+### FullText Search in Neo4J
+The code can run as a script, or be imported as a function. It should be run from the base directory 
+and expects the `data/` directory to already exist. 
+
+The `.env` file should be in the root directory and needs the following items set:
+```dotenv
+NEO4J_URI="bolt://localhost:7687"
+NEO4J_USERNAME="neo4j"
+NEO4J_PASSWORD=""
+SEARCH_NAMES_FILE="data/search_names.txt"
+SEARCH_OUTPUT_FULLTEXT="data/search_fulltext_output.csv"
+```
+I've left the least sensitive values in, so potentially this could be run with just a password added.
+
+The list of names should include one item to search per line. Overly common words will inflate the 
+result and can usually be left out. For example `Jacob` is very common in the database, but `MacAdam` 
+is not. Searching for `Jacob MacAdam` returns a lot of uninteresting results and searching for `MacAdam`
+gives better results.
--- src/fulletext_search.py
+++ src/fulletext_search.py
@@ -10,7 +10,14 @@ CONFIG = dotenv_values()
 LeakNode = namedtuple("LeakNode", "searched_name,found_name,node_id,score")
 
 
-def get_names(tx, names: Optional[list[str]] = None):
+def get_names(tx, names: Optional[list[str]] = None) -> None:
+    """
+    The transactions to search the database are performed here and written to
+    a csv file.
+
+    :param tx: The database session to use for searching
+    :param names: Optional list of names to search for
+    """
     if not names:
         names = Path(CONFIG["SEARCH_NAMES_FILE"]).read_text().splitlines()
     query = (
@@ -27,7 +34,19 @@ def get_names(tx, names: Optional[list[str]] = None):
 
 
 def process_names(names: Optional[list[str]] = None, uri=CONFIG["NEO4J_URI"], user=CONFIG["NEO4J_USERNAME"],
-                  password=CONFIG["NEO4J_PASSWORD"]):
+                  password=CONFIG["NEO4J_PASSWORD"]) -> None:
+    """
+    Search for names using a fulltext index in Neo4J. This function sets up
+    the driver and session for interacting with the database.
+
+    Names can optionally be passed directly to this function for search, or
+    add to a file that will be read in the actual search function.
+
+    :param names: Optional list of names to search for
+    :param uri: URI to connect to the database
+    :param user: The username for connecting to the database
+    :param password: The password that goes with the user name
+    """
     with GraphDatabase.driver(uri, auth=(user, password)) as driver:
         with driver.session(database="neo4j") as session:
             session.execute_read(get_names, names=names)
