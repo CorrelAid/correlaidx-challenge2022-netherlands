7847384ed9b428915ce2d37f6217c95d7ca24864
David Valpey <greengargoyle@hotmail.com>
2023-01-10 18:29:08 +0100

    Save results to csv

--- src/fulletext_search.py
+++ src/fulletext_search.py
@@ -1,3 +1,4 @@
+import csv
 from collections import namedtuple
 from pathlib import Path
 from typing import Optional
@@ -6,10 +7,10 @@ from neo4j import GraphDatabase
 from dotenv import dotenv_values
 
 CONFIG = dotenv_values()
-LeakNode = namedtuple("LeakNode", "name,node_id,score")
+LeakNode = namedtuple("LeakNode", "searched_name,found_name,node_id,score")
 
 
-def get_names(tx, names: Optional[list[str]] = None) -> set[LeakNode]:
+def get_names(tx, names: Optional[list[str]] = None):
     if not names:
         names = Path(CONFIG["SEARCH_NAMES_FILE"]).read_text().splitlines()
     query = (
@@ -17,19 +18,19 @@ def get_names(tx, names: Optional[list[str]] = None) -> set[LeakNode]:
         'YIELD node, score '
         'RETURN node.name, node.node_id, score'
     )
-    nodes = set()
-    for name in names:
-        results = tx.run(query, name=name)
-        nodes.update(LeakNode(*result.values()) for result in results)
-    return nodes
+    with open(CONFIG["SEARCH_OUTPUT_FULLTEXT"], "w", newline="") as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter="|", )
+        csv_writer.writerow(LeakNode._fields)
+        for name in names:
+            results = tx.run(query, name=name)
+            csv_writer.writerows(LeakNode(name, *result.values()) for result in results)
 
 
 def process_names(names: Optional[list[str]] = None, uri=CONFIG["NEO4J_URI"], user=CONFIG["NEO4J_USERNAME"],
                   password=CONFIG["NEO4J_PASSWORD"]):
     with GraphDatabase.driver(uri, auth=(user, password)) as driver:
         with driver.session(database="neo4j") as session:
-            results = session.execute_read(get_names, names=names)
-            print("c")
+            session.execute_read(get_names, names=names)
 
 
 if __name__ == '__main__':
