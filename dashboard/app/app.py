3070b3e569761364a8ac32e86fa8a0193106e8fe
ajg54 <alexisgillett@hotmail.com>
2023-01-26 20:23:09 +0100

    Added a little more detail (found vs not-found) to the basic summary table.

--- dashboard/app/app.py
+++ dashboard/app/app.py
@@ -9,7 +9,6 @@ app = Dash(__name__, server=server)
 
 #load data
 nl_data = pd.read_csv(os.path.join(os.path.dirname(__file__),"data", "addresses_nl.csv"))
-nl_data = nl_data.dropna(subset=['longitude'])
 
 #define dashboard elements
 app.layout = html.Div([
@@ -22,6 +21,7 @@ app.layout = html.Div([
         inline=True
     ),
     dash_table.DataTable(data=nl_data.to_dict('records'), id='summary-table', fill_width=False),
+    #TODO: make a default (blank) table for the DataTable to avoid loading full deatils on first launch
     dl.Map(id="leaflet-map", style={'width': '1000px', 'height': '500px'}, center=[52, 4.88], zoom=10,
            children=[dl.TileLayer()]),
 ])
@@ -35,7 +35,9 @@ def display_map(leak):
     nl_data_selection = nl_data
     if leak != "All":
         nl_data_selection = nl_data_selection[nl_data_selection['leak'] == leak]
-    nl_data_summary = [{'label':'Found addresses', 'total' :nl_data_selection.shape[0]}]
+    nl_data_summary = [{leak: 'NLD addresses', 'total': nl_data_selection.shape[0]}]
+    nl_data_selection = nl_data_selection.dropna(subset=['longitude'])
+    nl_data_summary = nl_data_summary + [{leak: 'Coordinates found', 'total': nl_data_selection.shape[0]}]
     markers = [dl.Marker(dl.Tooltip(row["address"]), position=[row["latitude"], row["longitude"]]) for i, row in nl_data_selection.iterrows()]
     return [dl.TileLayer(), dl.LayerGroup(markers)], nl_data_summary
 
