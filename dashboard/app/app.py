60c1036c02b1775a26ced4f65a7e3724cb19325a
ajg54 <alexisgillett@hotmail.com>
2023-01-31 15:39:28 +0100

    Redesigned the dashboard to recognise markers on the map being clicked, because I couldn't find a nice way to do so using the previous list of markers approach. Instead, I build a geoJSON file with all the addresses gives as features which is then filtered using the options and hideout controls.

--- dashboard/app/app.py
+++ dashboard/app/app.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from dash import Dash, dcc, html, Input, Output, dash_table
 import dash_leaflet as dl
+from dash_extensions.javascript import assign
 import pandas as pd
 import os
 
 server = Flask(__name__)
 app = Dash(__name__, server=server)
 
-#load data
-nl_data = pd.read_csv(os.path.join(os.path.dirname(__file__),"data", "addresses_nl.csv"))
+# create javascript function that filters on leak
+geojson_filter = assign("function(feature, context){return context.props.hideout.includes(feature.properties.leak);}")
+filter_options = ["All", "Paradise Papers", "Offshore Leaks", "Bahamas Leaks", "Pandora Papers ", 'Panama Papers']
+
+
+# load data
+nl_data = pd.read_csv(os.path.join(os.path.dirname(__file__), "assets", "addresses_nl.csv"))
 
 #define dashboard elements
 app.layout = html.Div([
@@ -16,31 +22,44 @@ app.layout = html.Div([
     html.P("Select a data leak"),
     dcc.RadioItems(
         id='nl-map-leak',
-        options=["All", "Paradise Papers", "Offshore Leaks", "Bahamas Leaks", "Pandora Papers ", 'Panama Papers'],
+        options=filter_options,
         value="All",
         inline=True
     ),
-    dash_table.DataTable(data=nl_data.to_dict('records'), id='summary-table', fill_width=False),
-    #TODO: make a default (blank) table for the DataTable to avoid loading full deatils on first launch
-    dl.Map(id="leaflet-map", style={'width': '1000px', 'height': '500px'}, center=[52, 4.88], zoom=10,
-           children=[dl.TileLayer()]),
+    dash_table.DataTable(data=[], id='summary-table', fill_width=False),
+    html.Div(children=[
+        dl.Map(id="leaflet-map", style={'width': '1000px', 'height': '500px'}, center=[52, 4.88], zoom=10,
+           children=[dl.TileLayer(),
+                     dl.GeoJSON(url="/assets/addresses_nl.json", options=dict(filter=geojson_filter),
+                                hideout=filter_options, id='address-markers')]),
+        html.H3(children="Text", id='marker-text')
+    ], style={'display': 'flex', 'flex-direction': 'row'})
 ])
 
 #define dashboard control actions
 @app.callback(
-    Output("leaflet-map", "children"),
+    Output("address-markers", "hideout"),
     Output("summary-table", "data"),
     Input("nl-map-leak", "value"))
 def display_map(leak):
     nl_data_selection = nl_data
+    chosen_filter = filter_options
     if leak != "All":
         nl_data_selection = nl_data_selection[nl_data_selection['leak'] == leak]
+        chosen_filter = leak
     nl_data_summary = [{leak: 'NLD addresses', 'total': nl_data_selection.shape[0]}]
     nl_data_selection = nl_data_selection.dropna(subset=['longitude'])
     nl_data_summary = nl_data_summary + [{leak: 'Coordinates found', 'total': nl_data_selection.shape[0]}]
-    markers = [dl.Marker(dl.Tooltip(row["address"]), position=[row["latitude"], row["longitude"]]) for i, row in nl_data_selection.iterrows()]
-    return [dl.TileLayer(), dl.LayerGroup(markers)], nl_data_summary
+    return chosen_filter, nl_data_summary
+
+@app.callback(
+    Output("marker-text", "children"),
+    Input("address-markers", "click_feature"))
+def marker_text(feature):
+    if feature is not None:
+        return str(feature['properties']['name'])
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
+# note a bug in dash_leaflet means the hideout property will give an error if run in debug mode
